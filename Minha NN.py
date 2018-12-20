b9430ec2d3b7f46a4b0f184cc6f1968bb65c8536
Victor Hugo <42815419+vhrm19@users.noreply.github.com>
2018-12-20 11:04:39 -0200

    Update Minha NN.py

--- Minha NN.py
+++ Minha NN.py
@@ -15,35 +15,49 @@ Entrada = np.column_stack((Passageiros,Em_pe))
 Tempo_por_Passageiro = np.array(Tempo_por_Passageiro)
 
 def ReLU(x):
-    return np.tanh(x)
+    return x * (x > 0)
 
 def dReLU(x):
-    return 1 - np.tanh(x)**2
+    return 1 * (x > 0)
+
+def bias(x):
+    return np.column_stack((x, np.zeros(len(Entrada)).T))
 
 layers = [3, 2, 1]
 
-def Forward_Propagation(input, layers):
-    W = []
+W = []
+
+W.append(np.array([
+            [0.01, 0.05, 0.07],
+            [0.2, 0.041, 0.11],
+            [0.04, 0.56, 0.13]
+        ]))
+
+W.append(np.array([
+    [0.04, 0.78],
+    [0.4, 0.45],
+    [0.65, 0.23],
+    [0, 0] # Linha referente a bias/10
+]))
+
+W.append(np.array([
+    [0.04],
+    [0.41],
+    [0] # Linha referente a bias/10
+]))
+
+def Forward_Propagation(input, layers, W):
     Z = []
     a = []
-    a.append(input)
-    for i in range(len(layers)):
-        W.append(np.random.randn(len(a[i].T), layers[i]))
-        Z.append(np.dot(a[i], W[i]))
-        a.append(ReLU(Z[i]))
-    return a, W, Z
-
-def FP_withW(input, layers, W):
-    Z = []
-    a = []
-    a.append(input)
+    a.append(bias(input))
     for i in range(len(layers)):
         Z.append(np.dot(a[i], W[i]))
-        a.append(ReLU(Z[i]))
-    return a
+        a.append(bias(ReLU(Z[i])))
+    a[-1] = np.delete(a[-1], 1, 1)
+    return a, Z
 
-a, W, Z = Forward_Propagation(Entrada, layers)
-Cost = 0.5 * sum(Tempo_por_Passageiro - a[3])**2
+a, Z = Forward_Propagation(Entrada, layers, W)
+
 
 def Backpropagation(y, a, Z, W):
     Grad = []
@@ -51,18 +65,26 @@ def Backpropagation(y, a, Z, W):
     for i in range(3):
         if i == 0:
             d.append(np.multiply(y - a[3], -dReLU(Z[2])))
-        if i != 0:
-            d.append(np.multiply(np.dot(d[i-1], W[-i+3].T), dReLU(Z[-i+2])))
-        Grad.append(np.dot(a[-i+2].T, d[i]))
+            Grad.append(np.dot(a[2].T, d[0]))
+        if i == 1:
+            d.append(np.multiply(np.dot(d[i-1], W[-i+3].T), bias(dReLU(Z[-i+2]))))
+            Grad.append(np.dot(a[-i+2].T, np.delete(d[i], i, 1)))
+        if i == 2:
+            d.append(np.multiply(np.dot(np.delete(d[i-1], 2, 1), W[-i+3].T), bias(dReLU(Z[-i+2]))))
+            Grad.append(np.dot(a[-i+2].T, np.delete(d[i], i, 1)))
     return Grad
 
 Grad = Backpropagation(Tempo_por_Passageiro, a, Z, W)
-for i in range(len(W)):
-    W[i] = W[i] - (0.01/50)*Grad[-i+2]
+
+for j in range(600):
+    for i in range(len(W)):
+        W[i] = W[i] - (0.01/50)*Grad[-i+2]
+    a, Z = Forward_Propagation(Entrada, layers, W)
+    Grad = Backpropagation(Tempo_por_Passageiro, a, Z, W)
 
 def Gradient_checking(W, Grad):
-    Vpos = W
-    Vneg = W
+    Vpos = np.array(W)
+    Vneg = np.array(W)
     loss2 = []
     loss1 = []
     h = 0.0001
@@ -73,17 +95,18 @@ def Gradient_checking(W, Grad):
                 V1.append([Grad[i][j][k]])
     for i in range(len(W)):
         for j in range(len(W[i])):
-            Vpos = W
-            Vneg = W
+            Vpos = np.array(W)
+            Vneg = np.array(W)
             for k in range(len(W[i][j])):
                 Vpos[i][j][k] = W[i][j][k] + h
-                a = FP_withW(Entrada, layers, Vpos)
+                a, lixo = Forward_Propagation(Entrada, layers, Vpos)
                 loss2.append(0.5 * sum(Tempo_por_Passageiro - a[3])**2)
                 Vneg[i][j][k] = W[i][j][k] - h
-                a = FP_withW(Entrada, layers, Vneg)
+                a, lixo = Forward_Propagation(Entrada, layers, Vneg)
                 loss1.append(0.5 * sum(Tempo_por_Passageiro - a[3])**2)
     V2 = (np.array(loss2) - np.array(loss1)) / (2 * h)
-    return (V1 - V2) / (V1 + V2)
+    print(np.array(loss2), "\n\n", np.array(loss1), "\n\n")
+    print(np.abs(V1 - V2))
+    return np.abs(V1 - V2) / np.abs(V1 + V2)
 
 Check = Gradient_checking(W, Grad)
-print(Check)
