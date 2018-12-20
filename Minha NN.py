317e02e2e4210316aa2bf7dd7bea756f1c9a651a
Victor Hugo <42815419+vhrm19@users.noreply.github.com>
2018-12-20 15:05:43 -0200

    Update Minha NN.py

--- Minha NN.py
+++ Minha NN.py
@@ -20,12 +20,14 @@ def ReLU(x):
 def dReLU(x):
     return 1 * (x > 0)
 
-def bias(x):
-    return np.column_stack((x, np.zeros(len(Entrada)).T))
+def bias(x, tam): # Adiciona uma coluna de zeros de linhas tamanho "tam" na entrada dada
+    return np.column_stack((x, np.zeros(len(tam)).T))
 
-layers = [3, 2, 1]
+layers = [3, 2, 1] # Layers 1, 2 e 3, com 3, 2 e 1(saida) neuronios respectivamente
 
-W = []
+lamb = 0.1 # Parametro lambda da Regularização L2
+
+W = [] # Pesos aleatórios iniciais
 
 W.append(np.array([
             [0.01, 0.05, 0.07],
@@ -49,60 +51,42 @@ W.append(np.array([
 def Forward_Propagation(input, layers, W):
     Z = []
     a = []
-    a.append(bias(input))
+    a.append(bias(input, input)) # Adiciona uma coluna de zeros na entrada
     for i in range(len(layers)):
-        Z.append(np.dot(a[i], W[i]))
-        a.append(bias(ReLU(Z[i])))
-    a[-1] = np.delete(a[-1], 1, 1)
+        Z.append(np.dot(a[i], W[i])) # Calcula a entrada * pesos
+        a.append(bias(ReLU(Z[i]), input)) # Aplica a função de ativação
+    a[-1] = np.delete(a[-1], 1, 1) # Deleta a coluna extra do bias
     return a, Z
 
 a, Z = Forward_Propagation(Entrada, layers, W)
 
-def Backpropagation(y, a, Z, W):
+def Backpropagation(y, A, z, w):
     Grad = []
     d = []
-    for i in range(3):
+    for i in range(3): # Calcula as derivadas parciais (gradientes) referentes a cada layer
         if i == 0:
-            d.append(np.multiply(y - a[3], -dReLU(Z[2])))
-            Grad.append(np.dot(a[2].T, d[0]))
+            d.append(np.multiply(y - A[3], -dReLU(Z[2])))
+            Grad.append(np.dot(A[2].T, d[0]) - lamb*w[-i+2]) # 
         if i == 1:
-            d.append(np.multiply(np.dot(d[i-1], W[-i+3].T), bias(dReLU(Z[-i+2]))))
-            Grad.append(np.dot(a[-i+2].T, np.delete(d[i], i, 1)))
+            d.append(np.multiply(np.dot(d[i-1], w[-i+3].T), bias(dReLU(z[-i+2]), y)))
+            Grad.append(np.dot(A[-i+2].T, np.delete(d[i], i, 1)) - lamb*w[-i+2])
         if i == 2:
-            d.append(np.multiply(np.dot(np.delete(d[i-1], 2, 1), W[-i+3].T), bias(dReLU(Z[-i+2]))))
-            Grad.append(np.dot(a[-i+2].T, np.delete(d[i], i, 1)))
+            d.append(np.multiply(np.dot(np.delete(d[i-1], 2, 1), w[-i+3].T), bias(dReLU(z[-i+2]), y)))
+            Grad.append(np.dot(A[-i+2].T, np.delete(d[i], i, 1)) - lamb*w[-i+2])
     return Grad
 
 Grad = Backpropagation(Tempo_por_Passageiro, a, Z, W)
 
-for j in range(60000):
+for j in range(5000): # Faz o Backpropagation minimizando a função custo: 0.5 * sum(y-ŷ)**2
     for i in range(len(W)):
         W[i] -= (0.01/50)*Grad[-i+2]
     a, Z = Forward_Propagation(Entrada, layers, W)
     Grad = Backpropagation(Tempo_por_Passageiro, a, Z, W)
 
-def Gradient_checking(W, Grad):
-    loss2 = []
-    loss1 = []
-    V1 = []
-    h = 0.0001
-    for i in range(len(Grad)):
-        for j in range(len(Grad[i])):
-            for k in range(len(Grad[i][j])):
-                V1.append([Grad[i][j][k]])
-    for i in range(len(W)):
-        for j in range(len(W[i])):
-            Vpos = W
-            Vneg = W
-            for k in range(len(W[i][j])):
-                Vpos[i][j][k] += h
-                a, Z = Forward_Propagation(Entrada, layers, Vpos)
-                loss2.append(0.5 * sum(Tempo_por_Passageiro - a[-1])**2)
-                Vneg[i][j][k] -= 2*h
-                a, Z = Forward_Propagation(Entrada, layers, Vneg)
-                loss1.append(0.5 * sum(Tempo_por_Passageiro - a[-1])**2)
-    V2 = (np.array(loss2) - np.array(loss1)) / (2 * h)
-    print(np.abs(V1-V2) / np.abs(V1+V2))
+def Predict(Passageiros, Cheio):
+    entrada = np.column_stack((Passageiros, Cheio))
+    Out, nd = Forward_Propagation(entrada, layers, W)
+    print("Tempo por Passageiro:", float(Out[-1]))
 
-
-Gradient_checking(W, Grad)
+# Entrada sendo (Numero de Passageiros no Ponto, O quao cheio esta o onibus: pouco, medio ou muito [1,2 ou 3])
+Predict(1,2)
