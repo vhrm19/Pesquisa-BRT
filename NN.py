d815aa43249c3a7616e7ede382b87089d6f3e376
Victor Hugo <42815419+vhrm19@users.noreply.github.com>
2019-01-26 09:00:08 -0600

    Update NN.py

--- NN.py
+++ NN.py
@@ -12,18 +12,20 @@ class NeuralNetwork():
 
         Loss: MSE, MAE, MSPE, MSLE, squared_hinge, hinge, categorical_hinge, logcosh, categorical_crossentropy,
               parse_categorical_crossentropy, binary_crossentropy, kullback_leibler_divergence, poisson, cosine_proximity.
-        """
+        """        
+        cost = 0
         self.Model = kr.Sequential([kr.layers.Dense(3, input_shape = (2,), activation = activation),
                                     kr.layers.Dense(2, activation = activation),
                                     kr.layers.Dense(1, activation = 'linear')])
         self.Model.compile(loss = 'MSE', optimizer= optimizer, metrics = ['acc', 'mae'])
-        self.Model.fit(Input, Output, epochs = epochs, verbose = 0)
-        evaluate =  self.Model.evaluate(Input, Output, verbose = 0)
-        print('Loss:', evaluate[0], 'Accuracy:', evaluate[1], 'MAE:', evaluate[2])
 
-        h = np.dot(np.dot(Input, np.linalg.inv(np.dot(Input.T, Input))), Input.T)
-        cost = sum(sum(((self.Model.predict(np.array(Input)) - Output) / (1 - h))**2) / len(Input)) / len(Input)
-        print("LOOCV:", cost)
+        for i in range(len(Input)):          
+            self.Model.fit(np.delete(Input, i, 0), np.delete(Output, i, 0), epochs = epochs, verbose = 0)
+            cost += (Output[i] - self.Model.predict(np.array([Input[i]])))**2
+        evaluate = self.Model.evaluate(Input, Output, verbose = 0)
+
+        print('Loss:', evaluate[0], 'Accuracy:', evaluate[1], 'MAE:', evaluate[2])
+        print("LOOCV:", cost / len(Input))
    
     def Predict(self, X, Y):
         print("Result:", float(self.Model.predict(np.array([[X,Y]]))))
