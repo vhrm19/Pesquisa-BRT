f9538c9762bfa1ccc905f5b1bd8984526ad0ec19
Victor Hugo <42815419+vhrm19@users.noreply.github.com>
2019-01-30 12:42:11 -0600

    Update NN GWO.py

--- NN GWO.py
+++ NN GWO.py
@@ -33,37 +33,74 @@ class Neural_Network:
             a.append(self.sigma(Z[i]))
         return a
 
+    def Reshape(self, Weight):
+        W = []
+        shape = len(self.Input.T)
+        start = 0
+        for i in range(len(self.layers)):
+            if i == 0:
+                stop = (len(self.Input.T) + 1) * self.layers[0]
+                W.append(np.reshape(Weight[start : stop], [shape + 1, self.layers[i]]))
+            else:
+                stop += (self.layers[i-1]+1) * self.layers[i]
+                W.append(np.reshape(Weight[start : stop], [shape + 1, self.layers[i]]))
+            shape = self.layers[i]
+            start = stop
+        return W
         
     def MSE(self, W):
+        if len(W) != len(self.layers):
+            W = self.Reshape(W)
         y_hat = self.Forward_Propagation(self.Input, W)
         return 1/ len(self.Output) * sum((self.Output - y_hat[-1])**2)
 
     def GWO(self):
-        Agents = 5
+        Agents = 10
 
         y = []
         x = []
         W = []
-        
-        W_fit = np.zeros(Agents)
-        Alpha = np.zeros_like(self.Random_Weights())
-        Alpha_score=float("inf")
-        Beta = np.zeros_like(self.Random_Weights())
-        Beta_score=float("inf")
-        Delta = np.zeros_like(self.Random_Weights())
-        Delta_score=float("inf")
+        Pos = []
 
         for i in range(Agents):
-            np.random.seed(i)
+            np.random.seed(3)
             W.append(self.Random_Weights())
             x.append([])
+            for m in range(len(W[i])):
+                for n in range(len(W[i][m])):
+                    for o in range(len(W[i][m][n])):
+                        Pos.append(W[i][m][n][o])
+        Pos = np.split(np.array(Pos), Agents)
         
+        W_fit = np.zeros(Agents)
+        Alpha = np.zeros_like(Pos[0])
+        Alpha_score=float("inf")
+        Beta = np.zeros_like(Pos[0])
+        Beta_score=float("inf")
+        Delta = np.zeros_like(Pos[0])
+        Delta_score=float("inf")
+
         for l in range(self.rate):
-            
-            a = 2 - l * 2 / self.rate
 
             for j in range(Agents):
-                for k in range(len(self.layers)):
+                W_fit[j] = self.MSE(Pos[j])
+
+                x[j].append(float(W_fit[j]))
+                
+                if W_fit[j] < Alpha_score:
+                    Alpha_score = W_fit[j]
+                    Alpha = Pos[j].copy()
+                if W_fit[j] > Alpha_score and W_fit[j] < Beta_score:
+                    Beta_score = W_fit[j]
+                    Beta = Pos[j].copy()
+                if W_fit[j] > Alpha_score and W_fit[j] > Beta_score and W_fit[j] < Delta_score:
+                    Delta_score = W_fit[j]
+                    Delta = Pos[j].copy()
+                
+            a = 2 - l * 2 / self.rate
+                
+            for j in range(Agents):
+                for k in range(len(Pos)):
 
                     r1= np.random.random()
                     r2= np.random.random()
@@ -71,7 +108,7 @@ class Neural_Network:
                     A1= 2* a* r1 -a
                     C1= 2* r2
 
-                    D_alpha = abs(C1 * Alpha[k] - W[j][k])
+                    D_alpha = abs(C1 * Alpha[k] - Pos[j][k])
                     X1 = Alpha[k] - A1 * D_alpha
 
                     r1= np.random.random()
@@ -80,7 +117,7 @@ class Neural_Network:
                     A2= 2* a* r1 -a
                     C2= 2* r2
 
-                    D_beta = abs(C2 * Beta[k] - W[j][k])
+                    D_beta = abs(C2 * Beta[k] - Pos[j][k])
                     X2 = Beta[k] - A2 * D_beta
 
                     r1= np.random.random()
@@ -89,25 +126,11 @@ class Neural_Network:
                     A3= 2* a* r1 -a
                     C3= 2* r2
 
-                    D_delta = abs(C3 * Delta[k] - W[j][k])
+                    D_delta = abs(C3 * Delta[k] - Pos[j][k])
                     X3 = Delta[k] - A3 * D_delta
 
-                    W[j][k] = (X1+X2+X3)/3
+                    Pos[j][k] = (X1+X2+X3)/3
 
-            for i in range(Agents):
-                W_fit[i] = self.MSE(W[i])
-
-                x[i].append(float(W_fit[i]))
-                
-                if W_fit[i] < Alpha_score:
-                    Alpha_score = W_fit[i]
-                    Alpha = W[i].copy()
-                if W_fit[i] > Alpha_score and W_fit[i] < Beta_score:
-                    Beta_score = W_fit[i]
-                    Beta = W[i].copy()
-                if W_fit[i] > Alpha_score and W_fit[i] > Beta_score and W_fit[i] < Delta_score:
-                    Delta_score = W_fit[i]
-                    Delta = W[i].copy()
             y.append(l)
 
         for i in range(Agents):
@@ -116,7 +139,7 @@ class Neural_Network:
 
         print('Cost:', float(Alpha_score))
 
-        return Alpha
+        return self.Reshape(Alpha)
 
     def Predict(self, Passageiros, Cheio):
         W = self.GWO()
