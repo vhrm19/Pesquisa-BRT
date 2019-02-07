3ddeeea71a738c131467299b5adc59fb1b04cec2
Victor Hugo <42815419+vhrm19@users.noreply.github.com>
2019-02-07 12:15:58 -0600

    Falta bias.

--- SVR Gurobi.py
+++ SVR Gurobi.py
@@ -4,55 +4,82 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from gurobipy import *
 
+class Kernels:
+        def __init__(self, opt, gamma, degree, constant):
+                self.opt = opt
+                self.gamma = gamma
+                self.degree = degree
+                self.constant = constant
+        
+        def Kernel(self, xi, xj):
+                if self.opt == 0: # RBF
+                        return np.exp(-self.gamma * np.linalg.norm(xi - xj)**2) 
+
+                if self.opt == 1: # Polynomial
+                        return pow(sum([xi[i] * xj[i] for i in range(len(xi))]) + self.constant, self.degree)
+
+                if self.opt == 2: # Linear
+                        return np.dot(xi, xj)
+
 class SVR:
-        def __init__(self, C = 0.1, e = 0.1):
+        def __init__(self, gamma = 0.1, degree = 3, constant = 0, C = 0.1, e = 0.1, kernel = 0):
                 self.C = C
-                self.e = e
-                
+                self.e = e     
+                self.Kernel = Kernels(kernel, gamma, degree, constant).Kernel
+
         def fit(self, Input, Output):
-                self.Input_std = Input
-                self.Input = ((Input - np.mean(Input, 0)) / np.std(Input, 0)).tolist()
+                self.Input_std = Input.copy()
+                self.Input = ((Input - np.mean(Input, 0)) / np.std(Input, 0))
                 self.Output = Output.tolist()
-                ksi = []
-                ksi_star = []
+                a = []
+                a_star = []
 
                 model = Model()
 
                 W = [   model.addVar(lb=-GRB.INFINITY, ub=GRB.INFINITY, name="W0"),
-                        model.addVar(lb=-GRB.INFINITY, ub=GRB.INFINITY, name="W1") ]
-                b = model.addVar(lb=-GRB.INFINITY, ub=GRB.INFINITY, name="bias")
+                        model.addVar(lb=-GRB.INFINITY, ub=GRB.INFINITY, name="W1"),
+                        model.addVar(lb=-GRB.INFINITY, ub=GRB.INFINITY, name="bias") ]
+
+                K = np.array([self.Kernel(np.asarray(self.Input[i]), np.asarray(self.Input[j]))
+                        for j in range(self.Input_std.shape[0])
+                        for i in range(self.Input_std.shape[0])]).reshape((self.Input_std.shape[0],self.Input_std.shape[0])).tolist()
 
                 for i in range(len(self.Input)):
-                        ksi.append(model.addVar(name=("ksi%d" % i)))
-                        ksi_star.append(model.addVar(name=("ksi_star%d" % i)))
+                        a.append(model.addVar(lb=0, name=("a")))
+                        a_star.append(model.addVar(lb=0, name=("a_star")))
 
-                obj = 0.5 * (W[0]*W[0] + W[1]*W[1]) + self.C*(quicksum(ksi) + quicksum(ksi_star))
+                obj = (-0.5* quicksum((a[i] - a_star[i]) * (a[j] - a_star[j]) * K[i][j] for i in range(len(self.Input)) for j in range(len(self.Input)))
+                        - self.e * quicksum(a[i] + a_star[i] for i in range(len(self.Input))) + quicksum(self.Output[i][0] * (a[i] - a_star[i]) for i in range(len(self.Input))))
 
-                model.setObjective(obj, GRB.MINIMIZE)
+                model.setObjective(obj, GRB.MAXIMIZE)
 
+                model.addConstr(quicksum(a[i] - a_star[i] for i in range(len(self.Input))) == 0, name=("c0"))
                 for i in range(len(self.Input)):
-                        model.addConstr(self.Output[i][0] - (W[0]*self.Input[i][0] + W[1]*self.Input[i][1]) - b <= self.e + ksi[i] , name=("c1%d" %i))
-                        model.addConstr(W[0]*self.Input[i][0] + W[1]*self.Input[i][1] + b - self.Output[i][0] <= self.e + ksi_star[i], name=("c2%d" %i))
-                        model.addConstr(ksi[i] and ksi_star[i] >= 0, name=("c3%d" %i))
+                        model.addConstr( 0 <= a[i] and a_star[i]  <= self.C, name=("c1%d" %i))
 
                 model.update()
                 model.optimize()
+                
+                self.a = []
+                self.a_star = []
+                for i in range(len(self.Input)):
+                        self.a.append(a[i].X)
+                        self.a_star.append(a_star[i].X)
 
-                results = {}
-                if model.Status == GRB.OPTIMAL:
-                        results['W'] = [W[0].X, W[1].X]
-                        results['bias'] = b.X
-                self.W = [W[0].X, W[1].X, b.X]
-                print(results)
+                self.W = sum((a[i].X - a_star[i].X) * np.asarray(self.Input[i]) for i in range(len(self.Input)))
+                print(self.W)
+
+        def Predict(self, x1, x2):
+                return sum((self.a[i] - self.a_star[i]) * self.Kernel(np.asarray(self.Input[i]), (x1,x2)) for i in range(len(self.Input)))
 
         def Graphs(self):
                 x = []
                 y = []
                 z = []
 
-                for i in range(1,10):
-                        for j in range(1,4):
-                                k = self.W[0]*i + self.W[1]*j + self.W[2]
+                for i in range(10):
+                        for j in range(4):
+                                k = self.Predict(i,j)
                                 x.append(i)
                                 y.append(j)
                                 z.append(k)
@@ -66,6 +93,16 @@ class SVR:
                 ax.plot_trisurf(x,y,z)
                 plt.legend()
                 plt.show()
+
+        def LOOCV(self):
+                print('Calculating Leave-one-out cross validation')
+                cost = 0
+                Input = self.Input.copy()
+                Output = self.Output.copy()
+                for i in range(len(Input)):
+                        self.fit(np.delete(Input, i, 0), np.delete(Output, i, 0))
+                        cost += (Output[i] - self.Predict(Input[i][0], Input[i][1]))**2
+                print("LOOCV:", float(cost / len(Input)))
 
 if __name__ == '__main__':
         np.random.seed(0)
